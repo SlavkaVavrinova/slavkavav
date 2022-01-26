8f0ed75f7ea9f8e0f3800bfb4a4323a5c2b26ead
Stanislava Vavřinová <slavka.vav@gmail.com>
2022-01-27 00:54:52 +0100

    build

--- src/Components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer class="footer">
-      <div class="footer__copyright">&copy; SlavkaVav.cz 2021</div>
-    </footer>
-  );
-};
-
-export default Footer;
